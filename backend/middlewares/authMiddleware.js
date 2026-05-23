9998110c30fc90245cb35c14a42b7cb59125a866
Khaswrang668 <khaswrangd668@gmail.com>
2026-05-23 16:47:52 +0530

    bug fix

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import { User } from "../models/user.model.js";
+import { Users } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
