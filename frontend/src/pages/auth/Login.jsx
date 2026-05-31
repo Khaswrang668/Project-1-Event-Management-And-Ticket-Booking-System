9a64dc3cae29a7b8182b2cf675c5a735ccb1a37a
Khaswrang668 <khaswrangd668@gmail.com>
2026-05-31 13:37:27 +0530

    added css changes

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -81,7 +81,7 @@ export default function Login() {
         </p>
         <p style={styles.footer} >
          Applying for admin access?{' '}
-        <Link to="/register-admin" style = "textDecoration: none; color: purple">
+        <Link to="/register-admin" style = "text-decoration: none; color: purple">
           Request here
         </Link>
        </p>
